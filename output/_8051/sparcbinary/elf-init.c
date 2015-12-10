606267cb41a358b222c18c1d09441a7c1d229112
51103220 <rewandedo1009@gmail.com>
2015-12-10 13:17:01 +0700

    INDIRECT FULL FILLED

--- output/_8051/sparcbinary/elf-init.c
+++ output/_8051/sparcbinary/elf-init.c
@@ -1,11 +1,10 @@
 // address: 0x10474
 int main(int argc, char *argv[], char *envp[]) {
     union { __size8 x; struct { __size8 bit1; __size8 bit2; __size8 bit3; __size8 bit4; __size8 bit5; __size8 bit6; __size8 bit7; __size8 bit8; } m; } a; 		// r8
-    __size8 a_1; 		// r8
-    __size8 r7; 		// r7
+    __size8 r0; 		// r0
 
-    a.x.m.bit8 = 1;
-    a_1 = cc(r7);
-    return a_1;
+    a.x.m.bit7 = 0;
+    *(__size8*)(r1) = r0 + 1;
+    return;
 }
 
--- output/_8051/sparcbinary/sparcbinary.c
+++
@@ -1,7 +0,0 @@
-__size8 cc(__size8 param1);
-
-// address: 0x10480
-__size8 cc(__size8 param1) {
-    return param1;
-}
-
