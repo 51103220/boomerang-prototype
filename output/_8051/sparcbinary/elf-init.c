7ec4bfd829e5cfe604ebc9d718a86be36488ba26
51103220 <rewandedo1009@gmail.com>
2015-12-18 19:08:44 +0700

    ORL ANL XRL indirect

--- output/_8051/sparcbinary/elf-init.c
+++ output/_8051/sparcbinary/elf-init.c
@@ -1,9 +1,10 @@
 // address: 0x10474
 int main(int argc, char *argv[], char *envp[]) {
     char a; 		// r8
+    unsigned char r0; 		// r0
 
-    *(__size8*)20 = 200;
-    a = 100 ^ *(__size8 *)20;
+    *(__size8*)(r0) = 200;
+    a = 100 ^ *(__size8 *)(r0);
     return a;
 }
 
